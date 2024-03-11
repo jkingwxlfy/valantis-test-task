22b614c9eed544a8e0a75502e3d5a0e6c89445eb
jkingwxlfy <danila.rikkoz26@gmail.com>
2024-03-11 09:28:53 +0500

    fix

---
+++ src/index.tsx
@@ -0,0 +1,4 @@
+import ReactDOM from 'react-dom/client';
+import App from './components/app/App.js';
+
+ReactDOM.createRoot(document.getElementById('root')!).render(<App />);
--- src/main.tsx
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import App from './components/app/App.jsx';
-
-ReactDOM.createRoot(document.getElementById('root')!).render(
-    <React.StrictMode>
-        <App />
-    </React.StrictMode>
-);
